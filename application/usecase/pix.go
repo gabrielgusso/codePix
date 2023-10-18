cc50f4a0f1b45555c52bffd55747cbfae244c994
Gabriel Gusso <gabriel.gusso@voluti.com.br>
2023-10-18 19:55:53 +0000

    application

---
+++ application/usecase/pix.go
@@ -0,0 +1,26 @@
+package usecase
+
+import "github.com/gabrielgusso/codePix-go/domain/model"
+
+type PixUseCast struct {
+	PixKeyRepository model.PixKeyRepositoryInterface
+}
+
+func (p *PixUseCase) RegisterKey(key string, kind string, accountId string) (*model.PixKey, error) {
+	account, err := p.PixKeyRepository.FindAccount(accountId)
+	if err != nil {
+		return nil, err
+	}
+
+	pixKey, err := model.NewPixKey(kind, account, key)
+	if err != nil {
+		return nil, err
+	}
+
+	p.PixKeyRepository.RegisterKey(pixKey)
+	if pixKey.ID == "" {
+		return nil, err
+	}
+
+	return pixKey, nil
+}
